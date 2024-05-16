6e5d9e58539b74c715216f39ea4ed2a88e978dc2
Ankit Baage <ankitbaage3@gmail.com>
2024-05-16 17:31:51 +0530

    advertsisement banner fixed-layout rewired

--- src/components/spares/SpareItem.js
+++ src/components/spares/SpareItem.js
@@ -9,44 +9,52 @@ export const SpareItem = ({ item }) => {
   };
   return (
     <div className={classes.container}>
-      <div className={classes.box} onClick={() => handleVrpDetail(item.id)}>
-        <div className={classes.box__img}>
-          <img
-            src={item.image}
-            alt={item.part_name}
-            className={classes.box_img_pic}
-          />
-        </div>
-        <div className={classes.box__info}>
-          <div className={classes.box__info__container}>
-            <h1 className={classes.box__info__title}>{item.part_name}</h1>
-            <Link className={classes.box__info__fav}>
-              <img src={fav} alt="fav" className={classes.box__info__fav__img} />
-            </Link>
+      <div className={classes.container__float}>
+        <div className={classes.box} onClick={() => handleVrpDetail(item.id)}>
+          <div className={classes.box__img}>
+            <img
+              src={item.image}
+              alt={item.part_name}
+              className={classes.box_img_pic}
+            />
           </div>
+          <div className={classes.box__info}>
+            <div className={classes.box__info__container}>
+              <h1 className={classes.box__info__title}>{item.part_name}</h1>
+              {/* <Link className={classes.box__info__fav}>
+                <img
+                  src={fav}
+                  alt="fav"
+                  className={classes.box__info__fav__img}
+                />
+              </Link> */}
+            </div>
 
-          <div className={classes.box__discount}>
-            <div className={classes.box__discount__container}>
+            <div className={classes.box__discount}>
               <h3 className={classes.box__discount__container__discountedPrice}>
                 Rs.{item.discounted_price}
               </h3>
 
               <h3 className={classes.box__discount__container__price}>
-                M.R.P.{item.original_price}
+                {item.original_price}
               </h3>
+              {/* </div> */}
+              <span className={classes.box__discount__img}>
+                {item.discount_percentage}% DISCOUNT
+              </span>
             </div>
-            <span className={classes.box__discount__img}>
-              {item.discount_percentage}% DISCOUNT
-            </span>
-          </div>
 
-          <div className={classes.box__info__btns}>
-            <button className={classes.box__info__btns__cart}>
-              Add to Cart
-            </button>
-            <button className={classes.box__info__btns__buy}>Buy Now</button>
+            <div className={classes.box__info__btns}>
+              <button className={classes.box__info__btns__cart}>
+                Add to Cart
+              </button>
+              <button className={classes.box__info__btns__buy}>Buy Now</button>
+            </div>
           </div>
         </div>
+        <Link className={classes.box__info__fav}>
+          <img src={fav} alt="fav" className={classes.box__info__fav__img} />
+        </Link>
       </div>
 
       <hr className={classes.box__item__divider} />
