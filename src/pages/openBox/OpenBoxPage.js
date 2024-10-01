98da7f7da4e1e04c4d223c16133d195c94298243
Ankit Baage <ankitbaage3@gmail.com>
2024-10-01 14:51:44 +0530

    bugs fixed

--- src/pages/openBox/OpenBoxPage.js
+++ src/pages/openBox/OpenBoxPage.js
@@ -4,7 +4,7 @@ import { SearchBar } from "../../components/ui/searchBarWithBackBtn/SearchBar";
 import { Outlet } from "react-router-dom";
 
 export const OpenBoxPage = () => {
-  const placeholder = "Search for new phones...";
+  const placeholder = "Search for open Box ...";
   return (
     <div className={classes.container}>
       <SearchBar placeholder={placeholder} />
